ab464188eb8ee65181674b449d139d1827eacd15
Spencer Rogers <spencer1248@gmail.com>
2013-10-22 11:12:48 -0400

    Add solution

--- app.rb
+++ app.rb
@@ -2,7 +2,8 @@ require_relative 'config/environment'
 
 class GameApp < Sinatra::Base
   get '/rps/:play' do
-    # Your app code goes here
+    @game = RPSGame.new(params[:play])
+
     erb :rps_game
   end
 end
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,2 +1,41 @@
 class RPSGame
+  attr_accessor :play, :computer_play
+
+  Plays = [:rock, :paper, :scissors]
+
+  WinMap = {
+    rock: :scissors,
+    paper: :rock,
+    scissors: :paper
+  }
+
+  def initialize(play)
+    play = play.to_sym if play
+
+    if RPSGame.valid_play?(play)
+      self.play = play
+      self.computer_play = Plays.sample
+    else
+      raise PlayTypeError
+    end
+  end
+
+  def self.valid_play?(play)
+    Plays.include?(play)
+  end
+
+  def won?
+    WinMap[play] == computer_play
+  end
+
+  def tied?
+    play == computer_play
+  end
+
+  def lost?
+    !won? && !tied?
+  end
+
+  class PlayTypeError < Exception
+  end
 end
